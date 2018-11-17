491119596ec6ecab7ea51725592d3c47c5c1357e
Angela Quiñones <dulange143@gmail.com>
2018-11-17 03:56:17 -0500

    Diseño de App sin terminar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
